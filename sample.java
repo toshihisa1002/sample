14fca95e2b56c68c9222975ecd9fa7c395479000
toshi1217 <b1595204@planet.kanazawa-it.ac.jp>
2016-06-30 18:01:38 +0900

    新しい機能

--- sample.java
+++ sample.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
 
 class sample {
+  public static ArrayList<String> name_list;
   public static void main(String[] args) {
-    System.out.println("hello world");
+    name_list = new ArrayList<String>();
+    add();
+    for (int i=0;i<name_list.size() ;i++ ) {
+      System.out.println(name_list.get(i));
+    }
+  }
+  public static void add(){
+    name_list.add("toshi");
   }
 }
